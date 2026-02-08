95f2476a545f624a701086ddfa997076c940e1b0
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2026-02-07 21:12:27 -0600

    data card component

--- SimulatorProject/src/App.vue
+++ SimulatorProject/src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import Cards from './components/cards.vue'
+
+// SVG icons
+const iconTrending = "M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+const iconMonitoing = "M22 12h-4l-3 9L9 3l-3 9H2"
+const iconMoinsture = "M12 2s6 7 6 11a6 6 0 1 1-12 0c0-4 6-11 6-11z"
+const iconWind = "M17.7 7.7A7.1 7.1 0 1 1 5 13.1M18 13.1h-8M6 13.1H2M14.5 17.5A3.5 3.5 0 0 1 11 21H2M10.5 8.5A3.5 3.5 0 1 1 14 5H2"
+
+
 </script>
 
 <template>
-  <Cards />
+  <Cards label="Producción Potencial" value="20.00" unit="m3" :iconPath="iconTrending"/>
+  <Cards label="Producción Potencial" value="20.00" unit="m3" :iconPath="iconTrending"/>
+  <Cards label="Producción Potencial" value="20.00" unit="m3" :iconPath="iconTrending"/>
 </template>
+
--- SimulatorProject/src/components/cards.vue
+++ SimulatorProject/src/components/cards.vue
@@ -1,18 +1,32 @@
 <script setup>
-import { ref } from 'vue'
 
-const titulo = ref('Componente con Tailwind')
+defineProps({
+  label: String,
+  value: String,
+  unit: String,
+  iconPath: String
+})
+
+
 </script>
 
 <template>
-  <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-    <div class="shrink-0">
-      <div class="h-12 w-12 bg-indigo-500 rounded-full"></div>
+  <div class="flex items-center gap-4 p-5 bg-white border border-gray-100 rounded-2xl shadow-sm w-full max-w-xs m-4">
+    
+    <div class="flex-shrink-0 flex items-center justify-center w-12 h-12 bg-[#4180ab]/10 rounded-xl">
+      <svg class="w-6 h-6 text-[#4180ab]" fill="none" stroke="currentColor" stroke-width="2.5" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" :d="iconPath" />
+      </svg>
     </div>
-    <div>
-      <div class="text-xl font-medium text-black">{{ titulo }}</div>
-      <p class="text-slate-500">¡Tailwind está funcionando!</p>
+
+    <div class="flex-1">
+      <p class="text-sm font-medium text-[#4180ab]/70">{{ label }}</p>
+      
+      <h3 class="text-2xl font-semibold text-[#4180ab] tracking-tight">
+        {{ value }} <span class="text-base align-top ml-0.5 font-bold">{{ unit }}</span>
+      </h3>
     </div>
+
   </div>
 </template>
 
