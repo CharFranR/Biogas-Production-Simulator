3ee47c8e1d37c1f307748d54566884481b7d053d
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2026-02-08 13:28:24 -0600

    add components

---
+++ BiosProject/src/components/buton.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+    buttonName: String,
+    iconPath: String
+})
+
+const emit = defineEmits(['click'])
+</script>
+
+<template>
+
+<div @click="$emit('click')" class="flex items-center justify-center shadow-sm w-full max-w-2xs  bg-[#4180ab]/80 rounded-2xl cursor-pointer">
+    
+    <div class="shrink-0 flex items-center justify-center w-12 h-12 rounded-xl">
+      <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" stroke-width="2.5" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" :d="iconPath" />
+      </svg>
+    </div>
+
+    <div class="flex text-white ">
+        {{buttonName}}
+    </div>
+</div>
+
+</template>
+
+<style scoped></style>
---
+++ BiosProject/src/components/container.vue
@@ -0,0 +1,17 @@
+<script setup >
+    defineProps ( {
+        label: String,
+        maxSize: { type: String, default: "" },
+    })
+</script>
+
+
+<template>
+  <div :class="['flex flex-col shadow-sm m-4 p-4 rounded-md', maxSize]">
+    <h2 class="text-[#4180ab]/70">
+      {{ label }}
+    </h2>
+
+    <slot />
+  </div>
+</template>
